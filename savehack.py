2b1562c5acb84e9a03433f9a13dd929dd23b4479
PotatoM12 <76020980+PotatoM12@users.noreply.github.com>
2020-12-15 12:17:01 +1100

    Update savehack.py

--- savehack.py
+++ savehack.py
@@ -2,19 +2,19 @@ import base64
 
 print("Idle Breakout Save Hack by Xsus")
 
-print("What level do you want to be on?")
+print("700000")
 level = input()
 
-print("What amount of money would you like to have")
+print("10000000000")
 money = input()
 
-print("How much gold do you want")
+print("10000000000")
 gold = input()
 
-print("How many Black Boxes?")
+print("270")
 bb = input()
 
-print("How many skillpoints")
+print("5760")
 sp = input()
 
 s = f"{level},{money},{gold},2,0,0,0,0,0,0,0,1,1,0,43595.78,999999,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,{bb},0,0,0,1,{sp},1,0,0"
